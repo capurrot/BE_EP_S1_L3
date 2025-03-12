69e2ba9c9dc5031fef18cbd2c7ab7e3c55cc7bd5
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-03-12 15:42:25 +0100

    Esercizio 1

---
+++ src/main/java/Esercizio1/Main.java
@@ -0,0 +1,11 @@
+package Esercizio1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Rettangolo.stampaRettangolo();
+        System.out.println("----------------------------------");
+        Rettangolo.stampaDueRettangoli();
+    }
+}
